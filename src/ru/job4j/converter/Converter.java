c8c3e18fd11de21d036998940f770163bf45aa5f
Bashnya202 <La141434>
2021-03-20 22:35:45 +0200

    4.1. Аргументы [#501934]

---
+++ src/ru/job4j/converter/Converter.java
@@ -0,0 +1,20 @@
+package ru.job4j.converter;
+
+public class Converter {
+
+    public static int rubleToEuro(int value){
+        int rsl = value/70;
+        return rsl;
+    }
+    public static int rubleToDolar (int value) {
+        int rsl = value/60;
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        int euro = Converter.rubleToEuro(210);
+        System.out.println("210 rub= " + euro + " euro");
+        int dollar = Converter.rubleToDolar(120);
+        System.out.println("120 rub = " + dollar +" dollar");
+    }
+}
