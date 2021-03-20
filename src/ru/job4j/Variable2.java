1a6fa93230b0f21863b2d379a7f66ec53d656ea9
Bashnya202 <La141434>
2021-03-20 21:57:02 +0200

    3. Арифметические операции. [#501936]

---
+++ src/ru/job4j/Variable2.java
@@ -0,0 +1,14 @@
+package ru.job4j;
+
+public class Variable2 {
+    public static void main(String[] args) {
+        byte age = 26;
+        char sex = 'M';
+        int growth = 174;
+        int numberBrick = 2_000_000;
+        long numberBacteria = 12_000_000L;
+        long distanceCosmicObject = 3_000_000;
+        boolean PermitWork = true;
+        double sizeFile = 23.332;
+    }
+}
